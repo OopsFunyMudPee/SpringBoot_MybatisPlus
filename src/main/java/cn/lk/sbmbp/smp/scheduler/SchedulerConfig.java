a8d20ed318e48ead3e428226362e24e1e65cae2e
liukai <liukai@gtmap.cn>
2021-06-10 16:46:21 +0800

    数据插入测试

--- src/main/java/cn/lk/sbmbp/smp/scheduler/SchedulerConfig.java
+++ src/main/java/cn/lk/sbmbp/smp/scheduler/SchedulerConfig.java
@@ -4,9 +4,10 @@ package cn.lk.sbmbp.smp.scheduler;
 import cn.lk.sbmbp.smp.mappers.UserMapper;
 import cn.lk.sbmbp.smp.po.User;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * @author 6k2
@@ -23,7 +24,7 @@ public class SchedulerConfig {
     //#   秒 分 时 ? * ?
     //每隔5秒执行一次 */20 * * * * ?
 
-    @Autowired
+    @Resource
     private UserMapper userMapper;
 
     @Scheduled(cron = "${localConfig.scheduler.fileuploadtime}")
